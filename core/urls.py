1965e193d4a5443b195e5b776457f8e00196ff95
Noor Mohammad <noormohammad.net1503011@gmail.com>
2022-05-03 03:32:15 +0600

    token

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from core.views import ProfileView, ActivateAccount, signup
+from core.views import ProfileView
 from . import views as function_views
 urlpatterns = [
     path('signup/', function_views.signup, name='signup'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
 
-    path('activate/<uidb64>/<token>/',
-         ActivateAccount.as_view(), name='activate'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+         function_views.activate, name='activate'),
 ]
--- core/views.py
+++ core/views.py
@@ -77,65 +77,83 @@ def signup(request):
 #     return render(request, 'commons/signup.html', {'form': form})
 
 
-class SignUpView(View):
-    form_class = SignUpForm
-    template_name = 'commons/signup.html'
+# class SignUpView(View):
+#     form_class = SignUpForm
+#     template_name = 'commons/signup.html'
 
-    def get(self, request, *args, **kwargs):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
+#     def get(self, request, *args, **kwargs):
+#         form = self.form_class()
+#         return render(request, self.template_name, {'form': form})
 
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
+#     def post(self, request, *args, **kwargs):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
 
-            user = form.save(commit=False)
-            user.is_active = False  # Deactivate account till it is confirmed
-            user.save()
+#             user = form.save(commit=False)
+#             user.is_active = False  # Deactivate account till it is confirmed
+#             user.save()
 
-            current_site = get_current_site(request)
-            subject = 'Activate Your MySite Account'
-            message = render_to_string('emails/account_activation_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            # Sending activation link in terminal
-            # user.email_user(subject, message)
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(subject, message, to=[to_email])
-            email.send()
+#             current_site = get_current_site(request)
+#             subject = 'Activate Your MySite Account'
+#             message = render_to_string('emails/account_activation_email.html', {
+#                 'user': user,
+#                 'domain': current_site.domain,
+#                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+#                 'token': account_activation_token.make_token(user),
+#             })
+#             # Sending activation link in terminal
+#             # user.email_user(subject, message)
+#             to_email = form.cleaned_data.get('email')
+#             email = EmailMessage(subject, message, to=[to_email])
+#             email.send()
 
-            messages.success(
-                request, ('Please Confirm your email to complete registration.'))
+#             messages.success(
+#                 request, ('Please Confirm your email to complete registration.'))
 
-            return redirect('login')
+#             return redirect('login')
 
-        return render(request, self.template_name, {'form': form})
+#         return render(request, self.template_name, {'form': form})
 
 
-class ActivateAccount(View):
+# class ActivateAccount(View):
 
-    def get(self, request, uidb64, token, *args, **kwargs):
-        try:
-            uid = force_str(urlsafe_base64_decode(uidb64))
-            user = User.objects.get(pk=uid)
-        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-            user = None
+#     def get(self, request, uidb64, token, *args, **kwargs):
+#         try:
+#             uid = force_str(urlsafe_base64_decode(uidb64))
+#             user = User.objects.get(pk=uid)
+#         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+#             user = None
 
-        if user is not None and account_activation_token.check_token(user, token):
-            user.is_active = True
-            user.profile.email_confirmed = True
-            user.save()
-            login(request, user)
-            messages.success(request, ('Your account have been confirmed.'))
-            return redirect('home')
-        else:
-            messages.warning(
-                request, ('The confirmation link was invalid, possibly because it has already been used.'))
-            return redirect('home')
+#         if user is not None and account_activation_token.check_token(user, token):
+#             user.is_active = True
+#             user.profile.email_confirmed = True
+#             user.save()
+#             login(request, user)
+#             messages.success(request, ('Your account have been confirmed.'))
+#             return redirect('home')
+#         else:
+#             messages.warning(
+#                 request, ('The confirmation link was invalid, possibly because it has already been used.'))
+#             return redirect('home')
 
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
+        user.save()
+        login(request, user)
+        messages.success(request, ('Your account have been confirmed.'))
+        return redirect('home')
+    else:
+        messages.warning(
+            request, ('The confirmation link was invalid, possibly because it has already been used.'))
+        return redirect('home')
 
 # Edit Profile View
 class ProfileView(UpdateView):
